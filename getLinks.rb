6f4ddb364bb69abb11c647d410c25399fe5a1a72
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2023-07-12 13:44:32 +0200

    Scrapping sur plusieurs pages

--- getLinks.rb
+++ getLinks.rb
@@ -2,26 +2,33 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-# ! Recuperation de tous les liens des annonces
-url = 'https://www.iadfrance.fr/annonces/nancy-54000/location'
+pages = (1..30).to_a
+
+# Recuperation de tous les liens des annonces
 domain_name = 'https://www.iadfrance.fr'
-html = URI.open(url)
-doc = Nokogiri::HTML(html)
+array_links = []
 
-# Supposons que vous souhaitiez extraire les titres des annonces
-link_annonce = doc.css('.i-card--title a').map { |link| link['href'] }
+pages.each do |page|
+sleep(1.5)
+  url = "https://www.iadfrance.fr/annonces/location?page=#{page}"
+  html = URI.open(url)
+  doc = Nokogiri::HTML(html)
 
+  # Supposons que vous souhaitiez extraire les titres des annonces
+  link_annonce = doc.css('.i-card--title a').map { |link| link['href'] }
 
-# Affichage des liens
-link_annonce.each do |link|
+  # Affichage des liens
+  link_annonce.each do |link|
     puts domain_name + link
+    array_links << domain_name + link
+  end
+end
 
-    # Enregistrement des liens dans un fichier CSV
-    CSV.open('annonces.csv', 'w', write_headers: true, headers: ['url']) do |csv|
-        link_annonce.each do |link|
-            csv << [domain_name + link]
-        end
-    end
+# Enregistrement des liens dans un fichier CSV
+CSV.open('annonces.csv', 'w', write_headers: true, headers: ['url']) do |csv|
+  array_links.each do |link|
+    csv << [link]
+  end
 end
 
 puts "Les liens des annonces ont été enregistrés dans le fichier annonces.csv."
